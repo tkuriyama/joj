007c715023375e5d25f7630bd1313e164404bf99
tkuriyama <taro.kuriyama@gmail.com>
2021-08-31 10:55:18 -0400

    Add memo version

--- snippets/day1/fibonacci.ts
+++ snippets/day1/fibonacci.ts
@@ -18,29 +18,39 @@ function fib_rec(n: number): number {
 
 
 function fib_helper(a: number, b: number, i: number, n: number): number {
-    if (i >= n) {
-        return b
-    }
-    else {
-        return fib_helper(b, a + b, i + 1, n)
-    }
-
+    return (i >= n ? b : fib_helper(b, a + b, i + 1, n))
 }
 
 
+
 function fib(n: number): number {
-    if (n <= 0) {
-        return n
+    return (n <= 0 ? n : fib_helper(0, 1, 1, n))
+}
+
+
+// Memoization
+
+
+function fibMemo(n: number): number {
+    const memo = [0, 1];
+    function f(n: number): number {
+        const result = memo[n]
+        if (typeof result !== 'number') {
+            const sum = f(n - 1) + f(n - 2);
+            memo[n] = sum
+            return sum
+        }
+        else {
+            return result
+        }
     }
-    else {
-        return fib_helper(0, 1, 1, n)
-    }
+    return f(n)
 }
 
 
 // Equality Test
 
 
-function testFib(n: number): boolean {
-    return fib(n) === fib_rec(n)
+function fibTest(n: number): boolean {
+    return fib(n) === fib_rec(n) && fib(n) === fibMemo(n)
 }
