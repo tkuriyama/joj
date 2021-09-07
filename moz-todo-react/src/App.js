bdb794fcde1371993c77a1fc985a5fd6387a3f3e
tkuriyama <taro.kuriyama@gmail.com>
2021-09-07 19:00:04 -0400

    Add add and delete features

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 
 export default App;
@@ -9,19 +10,51 @@ export default App;
 /*----------------------------------------------------------------------------*/
 
 function App(props) {
-    
-    const taskList = props.tasks.map(task =>
+
+    const [tasks, setTasks] = useState(props.tasks);
+
+    function addTask(name) {
+        const newTask = {
+            id: "todo-" + nanoid(),
+            name: name,
+            completed: false
+        };
+        setTasks([...tasks, newTask]);
+    };
+
+    const taskList = tasks.map(task =>
         <Todo
         name={task.name}
         id={task.id}
         completed={task.completed}
         key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
         />);
+
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map(task => {
+            return (task.id === id) ? {...task, completed: !task.completed} : task
+        });
+        setTasks(updatedTasks);
+    }
+
+    function deleteTask(id) {
+        const updatedTasks = tasks.filter(task => {
+            return (task.id !== id)
+        });
+        setTasks(updatedTasks);
+    }
+
+
+    const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
     return (
         <div className="todoapp stack-large">
         <h1>TodoMatic</h1>
-        <Form />
+        <Form onSubmit={addTask}/>
 
         <div className="filters btn-group stack-exception">
         <FilterButton />
@@ -30,7 +63,7 @@ function App(props) {
         </div>
 
         <h2 id="list-heading">
-        {taskList.length} tasks remaining
+        {headingText}
         </h2>
 
         <ul className="todo-list stack-large stack-exception"
--- moz-todo-react/src/components/Form.js
+++ moz-todo-react/src/components/Form.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 /*----------------------------------------------------------------------------*/
 
 export default function Form(props) {
-  return (
-    <form>
-      <h2 className="label-wrapper">
+    const [name, setName] = useState("");
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (name !== "") {
+            props.onSubmit(name);
+            setName("");
+        };
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+        <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
+        What needs to be done?
         </label>
-      </h2>
-      <input
+        </h2>
+        <input
         type="text"
         id="new-todo-input"
         className="input input__lg"
         name="text"
         autoComplete="off"
-      />
-      <button type="submit" className="btn btn__primary btn__lg">
+        value={name}
+        onChange={handleChange}
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
         Add
-      </button>
-    </form>
-  );
+        </button>
+        </form>
+    );
 }
